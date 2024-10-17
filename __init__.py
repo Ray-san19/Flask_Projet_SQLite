9c48a2c084726cdc0a78b57d3c0c268ba4a715d2
Ray-san19 <rayanemoutairou@gmail.com>
2024-10-17 16:16:56 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -84,7 +84,11 @@ def enregistrer_client():
 
 @app.route('/fiche_nom/<post_nom>')
 def fiche_nom(post_nom):
-    if not estauthentifie():
+     if not estauthentifie():
+        # Rediriger vers la page d'authentification si l'utilisateur n'est pas authentifié
+        return redirect(url_for('authentification'))
+
+     else
         conn = sqlite3.connect('database.db')
         cursor = conn.cursor()
         cursor.execute('SELECT * FROM clients WHERE Nom = ?', (post_nom,))
