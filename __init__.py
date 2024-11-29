50aadf1a9b880aecc7e0e54b548d87776449d567
Ray-san19 <rayanemoutairou@gmail.com>
2024-11-29 16:23:24 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -155,7 +155,7 @@ def emprunter_livre():
         livre_id = request.form['livre_id']
         conn = sqlite3.connect('database.db')
         cursor = conn.cursor()
-        cursor.execute('INSERT INTO loans (user_id, livre_id) VALUES (?, ?)', (user_id, livre_id))
+        cursor.execute('INSERT INTO emprunt (user_id, livre_id) VALUES (?, ?)', (user_id, livre_id))
         conn.commit()
         conn.close()
         return redirect(url_for('consultation_livres_emprunt'))
