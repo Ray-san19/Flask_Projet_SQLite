feb2a5ab5e8329713cb439c2f886db56876d2765
Ray-san19 <rayanemoutairou@gmail.com>
2024-12-01 14:41:50 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -89,7 +89,7 @@ def enregistrer_client():
     cursor = conn.cursor()
 
     # Exécution de la requête SQL pour insérer un nouveau client
-    cursor.execute('INSERT INTO clients (created, nom, prenom, adresse) VALUES (?, ?, ?, ?)', (CURRENT_DATE, nom, prenom, "ICI"))
+    cursor.execute('INSERT INTO clients (created, nom, prenom, adresse) VALUES (?, ?, ?, ?)', ("CURRENT_DATE", nom, prenom, "ICI"))
     conn.commit()
     conn.close()
     return redirect('/consultation/')  # Rediriger vers la page d'accueil après l'enregistrement
