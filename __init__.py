cb1bd3bc0062fb75792dceb34fe737cba80f80b7
Ray-san19 <rayanemoutairou@gmail.com>
2025-01-05 16:49:36 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -165,7 +165,7 @@ def emprunter_livre():
 # --- Nouvelle route pour retourner un livre ---
 @app.route('/retourner/<int:id>')
 def retourner_livre(id):
-    if estauthentifie():
+    if estauthentifie() and request.method == 'POST':
         livre_id = request.form['livre_id']
         conn = sqlite3.connect('database.db')
         cursor = conn.cursor()
